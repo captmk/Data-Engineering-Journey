f09c133e34658dc87a3aa83041870f2bfdfff6f1
Keerthi Prasad Kalluraya M <121145797+captmk@users.noreply.github.com>
2026-07-04 22:54:07 +0530

    initainl comot

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mkdir -p \
+Python/Day01 \
+Python/Day02 \
+Python/Projects \
+SQL/Day01 \
+SQL/Day02 \
+SQL/Projects \
+Git \
+Linux \
+Cloud \
+Spark \
+Airflow \
+Projects/Project01 \
+Projects/Project02 \
+Interview \
+Resources
+
+touch \
+README.md \
+Python/Day01/day01.py \
+Python/Day01/notes.md \
+Python/Day01/homework.md \
+Python/Day02/day02.py \
+Python/Day02/notes.md \
+Python/Day02/homework.md
+
+echo "✅ Data Engineering Journey created successfully!"
