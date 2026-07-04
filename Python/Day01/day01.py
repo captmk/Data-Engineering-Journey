fba883465b75272730264f656d4a4df74dd61e88
Keerthi Prasad Kalluraya M <121145797+captmk@users.noreply.github.com>
2026-07-04 22:59:01 +0530

    Day2

---
+++ Python/Day01/day01.py
@@ -0,0 +1,9 @@
+name = "keerthi";
+age = 22;
+company = "Oitro";
+salary = 15000;
+months_worked = 6;
+dream_salary = 100000;
+current_salary = 15000;
+
+print("Name:", name,"Age:", age,"Company:", company,"Salary:", salary,"Months Worked:", months_worked, "Dream Salary:", dream_salary,"Current Salary:", current_salary  );
---
+++ Python/Day02/day02.py
@@ -0,0 +1,20 @@
+name = "Keerthi"
+age = 23
+salary = 15000
+height = 174
+is_employee = True
+
+print (type(name))
+print (type(age))
+print (type(salary))
+print (type(height))
+print (type(is_employee))
+
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+Dream_salary = float(input("Enter your dream salary: "))
+
+print ("Welcome", name)
+print ("This year you are", age, "years old")
+print ("Your dream salary is", Dream_salary)
