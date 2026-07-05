f4a6843d34a0ac72941ff5a8e0687dee0c76a33d
Keerthi Prasad Kalluraya M <121145797+captmk@users.noreply.github.com>
2026-07-05 23:17:49 +0530

    Day3 commit

---
+++ Python/Day03/day03.py
@@ -0,0 +1,41 @@
+
+name = input("Enter your Name: ")
+salary = int(input("Enter your Current Salary: "))
+bonus = int(input("Whats your bonus this month ? "))
+
+new_salary = salary + bonus
+print ("So your new salary is : ", new_salary)
+
+if new_salary > 20000:
+    print("Congratulations")
+else:
+    print("Keep learning")
+
+
+
+# Homework A #
+
+20 + 10 
+20 - 10 
+20 * 10
+20 / 10
+20 // 3
+20 % 3
+2 ** 5
+
+print(20 + 10, 20 - 10, 20 * 10, 20 / 10, 20 // 3, 20 % 3, 2 ** 5)
+
+# Homework B #
+salary = 15000
+
+salary += 5000
+print(salary)
+
+
+salary -=5000
+print(salary)
+
+
+salary *= 2
+print(salary)
+
