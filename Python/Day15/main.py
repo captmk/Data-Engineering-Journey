311980f63b56447fb27c10dfe852ab7418d90b42
Keerthi Prasad Kalluraya M <121145797+captmk@users.noreply.github.com>
2026-07-22 13:49:25 +0530

    Complete Python Lesson 15 - Modules & Packages

---
+++ Python/Day15/main.py
@@ -0,0 +1,9 @@
+import calculator as c 
+a = int(input("\nEnter the fist no. : "))
+b = int(input("\n Enter the second number : "))
+
+print(c.add(a,b))
+print(c.subtract(a,b))
+print(c.multiply(a,b))
+print(c.divide(a,b))
+
---
+++ Python/Day15/trial/day15.py
@@ -0,0 +1,4 @@
+import Python.Day15.trial.calculator as calculator
+
+print(calculator.add(10, 20))
+print(calculator.multiply(5, 4))
